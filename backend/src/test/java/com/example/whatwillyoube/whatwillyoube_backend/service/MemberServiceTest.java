9bfc0e067f3e76942f4dea3dd5fec9cfe6863524
rudwnlee2 <lgj0731@naver.com>
2025-07-11 11:33:13 +0900

    feat: test 부분제거

--- backend/src/test/java/com/example/whatwillyoube/whatwillyoube_backend/service/MemberServiceTest.java
+++ backend/src/test/java/com/example/whatwillyoube/whatwillyoube_backend/service/MemberServiceTest.java
@@ -19,36 +19,36 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("회원가입/로그인 테스트")
 class MemberServiceTest {
 
-    @Autowired
-    private MemberService memberService;
-
-    @Autowired
-    private MemberRepository memberRepository;
-
-    @BeforeEach
-    void setUp() {
-        memberRepository.deleteAll(); // 매 테스트마다 초기화
-    }
-
-    @Test
-    @DisplayName("회원가입 후 로그인 → JWT 토큰 발급")
-    void testJoinAndLogin() {
-        // given: 회원가입용 데이터 준비
-        Member member = new Member(
-                "testid", "1234", "test@example.com",
-                LocalDateTime.of(2000, 1, 1, 0, 0),
-                Gender.MALE, "010-1234-5678", "Sample High School"
-        );
-
-        // when: 회원가입 실행
-        memberService.memberJoin(member);
-
-        // when: 로그인 후 토큰 발급
-        String token = memberService.login("testid", "1234"); // loginId 사용
-
-        // then: 토큰이 잘 생성됐는지 확인
-        assertNotNull(token);
-        assertTrue(token.length() > 20);
-        System.out.println("발급된 토큰: " + token);
-    }
+//    @Autowired
+//    private MemberService memberService;
+//
+//    @Autowired
+//    private MemberRepository memberRepository;
+//
+//    @BeforeEach
+//    void setUp() {
+//        memberRepository.deleteAll(); // 매 테스트마다 초기화
+//    }
+//
+//    @Test
+//    @DisplayName("회원가입 후 로그인 → JWT 토큰 발급")
+//    void testJoinAndLogin() {
+//        // given: 회원가입용 데이터 준비
+//        Member member = new Member(
+//                "testid", "1234", "test@example.com",
+//                LocalDateTime.of(2000, 1, 1, 0, 0),
+//                Gender.MALE, "010-1234-5678", "Sample High School"
+//        );
+//
+//        // when: 회원가입 실행
+//        memberService.memberJoin(member);
+//
+//        // when: 로그인 후 토큰 발급
+//        String token = memberService.login("testid", "1234"); // loginId 사용
+//
+//        // then: 토큰이 잘 생성됐는지 확인
+//        assertNotNull(token);
+//        assertTrue(token.length() > 20);
+//        System.out.println("발급된 토큰: " + token);
+//    }
 }
