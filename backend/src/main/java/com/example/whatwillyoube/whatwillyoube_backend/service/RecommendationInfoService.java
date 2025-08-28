31866249d786b705e0edab8019a84ad6d0343061
rudwnlee2 <lgj0731@naver.com>
2025-08-28 14:49:59 +0900

    feat: 트랜잭션 추가

--- backend/src/main/java/com/example/whatwillyoube/whatwillyoube_backend/service/RecommendationInfoService.java
+++ backend/src/main/java/com/example/whatwillyoube/whatwillyoube_backend/service/RecommendationInfoService.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class RecommendationInfoService {
 
     private final RecommendationInfoRepository recommendationInfoRepository;
