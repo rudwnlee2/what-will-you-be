50150fa086eadfb4a8fc48d322e8fa414f8d5c6b
BaekJae19 <bjy5792@gmail.com>
2025-09-19 16:03:18 +0900

    yarn dev 업데이트

--- frontend/WWUB/src/api/members.ts
+++ frontend/WWUB/src/api/members.ts
@@ -26,7 +26,7 @@ export const login = async (data: LoginData) => {
 
 /**
  * 3. 아이디 중복 확인
- * GET /api/members/check-loginid/{loginId}
+ * GET /api/members/check-username/{loginId}
  */
 export const checkLoginId = async (loginId: string): Promise<{ exists: boolean }> => {
   const response = await axiosInstance.get(`/api/members/check-username/${loginId}`);
