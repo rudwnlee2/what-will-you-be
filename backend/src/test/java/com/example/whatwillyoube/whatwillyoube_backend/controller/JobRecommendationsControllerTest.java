83ee0b75673d556feba22b4588be7b0940f991e4
rudwnlee2 <lgj0731@naver.com>
2025-09-26 12:18:33 +0900

    test: 진행중

--- backend/src/test/java/com/example/whatwillyoube/whatwillyoube_backend/controller/JobRecommendationsControllerTest.java
+++ backend/src/test/java/com/example/whatwillyoube/whatwillyoube_backend/controller/JobRecommendationsControllerTest.java
@@ -1,9 +1,12 @@
 package com.example.whatwillyoube.whatwillyoube_backend.controller;
 
-import com.example.whatwillyoube.whatwillyoube_backend.domain.Gender;
-import com.example.whatwillyoube.whatwillyoube_backend.domain.Member;
+import com.example.whatwillyoube.whatwillyoube_backend.domain.*;
+import com.example.whatwillyoube.whatwillyoube_backend.dto.MemberRequestDto;
+import com.example.whatwillyoube.whatwillyoube_backend.dto.RecommendationInfoRequestDto;
 import com.example.whatwillyoube.whatwillyoube_backend.repository.JobRecommendationsRepository;
 import com.example.whatwillyoube.whatwillyoube_backend.repository.MemberRepository;
+import com.example.whatwillyoube.whatwillyoube_backend.repository.RecommendationInfoRepository;
+import com.example.whatwillyoube.whatwillyoube_backend.util.JwtUtil;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
 
@@ -20,26 +24,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class JobRecommendationsControllerTest {
 
     @Autowired
-    JobRecommendationsRepository jobRecommendationsRepository;
+    private JobRecommendationsRepository jobRecommendationsRepository;
 
     @Autowired
-    MemberRepository memberRepository;
+    private RecommendationInfoRepository recommendationInfoRepository;
 
-    void saveMember() {
-        Member member = Member.builder()
-                .loginId("test123")
-                .password("qwe123!@#")
-                .name("tesst")
-                .email("test@test.com")
-                .birth(LocalDate.of(2001, 1, 31))
-                .gender(Gender.MALE)
-                .phone("010-1234-5678")
-                .school("한신대학교").build();
+    @Autowired
+    private MemberRepository memberRepository;
 
-        memberRepository.save(member);
-    }
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-//    void login
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    private Member member;
+    private String token;
 
     @LocalServerPort
     int port;
@@ -47,14 +47,75 @@ class JobRecommendationsControllerTest {
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
-        memberRepository.deleteAll();
+
         jobRecommendationsRepository.deleteAll();
+        recommendationInfoRepository.deleteAll();
+        memberRepository.deleteAll();
+
+        // 회원 생성
+        MemberRequestDto memberRequestDto = new MemberRequestDto(
+                "testId",
+                "Password!1",
+                "테스터",
+                "test@test.com",
+                LocalDate.of(2000, 1, 1),
+                Gender.MALE,
+                "010-1234-5678",
+                "테스트학교"
+        );
+
+        member = memberRepository.saveAndFlush(memberRequestDto.toEntity(passwordEncoder));
+
+        // 토큰 생성
+        token = jwtUtil.createToken(member.getEmail(), member.getName());
+
+        RecommendationInfoRequestDto recommendationInfoRequestDto = new RecommendationInfoRequestDto(
+                member.getId(),
+                "개발자",
+                "코딩을 좋아해서",
+                "IT",
+                JobValue.STABILITY,
+                MBTI.ISTJ,
+                "프로그래밍",
+                "수학",
+                Holland.INVESTIGATIVE
+        );
+
+        recommendationInfoRepository.saveAndFlush(RecommendationInfo.builder()
+                .member(member)
+                .dream(recommendationInfoRequestDto.getDream())
+                .dreamReason(recommendationInfoRequestDto.getDreamReason())
+                .interest(recommendationInfoRequestDto.getInterest())
+                .jobValue(recommendationInfoRequestDto.getJobValue())
+                .mbti(recommendationInfoRequestDto.getMbti())
+                .interest(recommendationInfoRequestDto.getInterest())
+                .holland(recommendationInfoRequestDto.getHolland())
+                .build());
+
     }
 
     @Test
     @DisplayName("추천에 필요한 정보 저장 성공")
     void createJobRecommendations_success() {
         
+    }
+
+    @Test
+    @DisplayName("추천 정보 리스트 조회 성공")
+    void getMyRecommendationsList_success() {
+
+    }
+
+    @Test
+    @DisplayName("내 직업 추천 기록 상세 조회 성공")
+    void getDetailRecommendation_success() {
+
+    }
+
+    @Test
+    @DisplayName("추천 기록 삭제 성공")
+    void deleteJobRecommendation_success() {
+
     }
 
 }
