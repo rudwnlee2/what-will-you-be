2f02c5fac3b851f4dfe3e9a7aa77f050ceda3f37
ccn1234 <scw1buee@naver.com>
2025-10-15 10:23:34 +0900

    오류 수정

--- recommender/config/urls.py
+++ recommender/config/urls.py
@@ -5,8 +5,6 @@ URL configuration for recommender.config project.
 from django.contrib import admin
 from django.urls import path, include   # ✅ include 추가
 
-app_name = 'recommend_app'  # ✅ 추가
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     # ✅ recommend_app 라우트 추가 → /api/ 밑으로 연결
--- recommender/recommend_app/urls.py
+++ recommender/recommend_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import RecommendAPIView
 
+app_name = 'recommend_app'  # ✅ 추가
+
 urlpatterns = [
     path('recommend/', RecommendAPIView.as_view(), name='recommend'),
 ]
